c156d19cbbcb15767749838a32fc1476d4338a7d
Vamshi Ratna <vamshiratna@Vamshis-MacBook-Air-2.local>
2025-11-19 21:13:43 -0500

    updated Resume

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -90,7 +90,7 @@ export default function Skills() {
         "MongoDB",
         "DynamoDB",
         "MySQL",
-        "AWS",
+        "Postman",
         "Git & GitHub",
         "Kubernetes",
         "Docker",
