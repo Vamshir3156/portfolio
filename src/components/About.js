5ccc8f2c5a85c3e924afe8cbf0f9db71f8b66da6
Vamshi Ratna <vamshiratna5@gmail.com>
2026-01-25 02:10:22 -0500

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,14 @@ export default function About() {
         About Me
       </h2>
       <p className={styles.aboutText}>
-        Hi, I'm Vamshi! — a dedicated Full Stack Developer with 3+ years of
-        experience crafting scalable, high-performance web applications. I
-        specialize in building intuitive frontends with React and robust
-        backends using Node.js and Java, often within microservices
-        architectures. I’m passionate about creating seamless user experiences,
-        writing clean, maintainable code, and collaborating across teams to
-        bring ideas to life. Always eager to learn and grow, I thrive in
-        fast-paced environments where I can explore new tools and technologies.
+        Hi, I'm Vamshi! — a dedicated Full Stack Developer with experience
+        crafting scalable, high-performance web applications. I specialize in
+        building intuitive frontends with React and robust backends using
+        Node.js and Java, often within microservices architectures. I’m
+        passionate about creating seamless user experiences, writing clean,
+        maintainable code, and collaborating across teams to bring ideas to
+        life. Always eager to learn and grow, I thrive in fast-paced
+        environments where I can explore new tools and technologies.
       </p>
     </section>
   );
